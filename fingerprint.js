f9c0269663b75a58bf65eda0e6d6b092c7165480
Anson Leung <ansonscleung@gmail.com>
2019-12-08 14:55:04 +0800

    Audio comparison with Hamming/Levenshtein distance

--- fingerprint.js
+++ fingerprint.js
@@ -7,6 +7,7 @@ async function fingerprint(audio) {
     } else {
         sRate[0] = audio[0].length*audio[0].sampleRate/audio[1].length;
     }
+    var peaks = [];
     $.each(audio, function(index, buffer){
         var offlineContext = new OfflineAudioContext(1, buffer.length, buffer.sampleRate);
         var source = offlineContext.createBufferSource();
@@ -19,11 +20,13 @@ async function fingerprint(audio) {
         source.start(0);
         offlineContext.startRendering();
         offlineContext.oncomplete = function(e) {
-            fingerprintProcess(e, sRate[index]).then(/*function(bpm){
-                console.log(file + ": " + bpm);
-                $(file).text(bpm);
-                return bpm;
-            }*/);
+            fingerprintProcess(e, sRate[index]).then(function(peak){
+                peaks.push(peak);
+                console.log(peaks.length);
+                if (peaks.length == 2)
+                    console.log("Hamming Distance: " + hammingDistance(peaks[0], peaks[1]));
+                    console.log("Levenshtein Distance: " + levenshteinDistance(peaks[0], peaks[1]));
+            });
         };
     });
 }
@@ -39,8 +42,9 @@ async function fingerprintProcess(e, sampleRate) {
 
     var threshold = min + (max - min) * .6;
     //console.log("threshold = " + threshold);
-    var peaks = getPeaksAtThreshold(data, threshold, sampleRate);
-    console.log("peaks = " + peaks.map(x => x/filteredBuffer.length));
+    var peaks = getPeaksArrayAtThreshold(data, threshold, sampleRate);
+    console.log("peaks = " + peaks);
+    return peaks;
 
     /*var intervalCounts = countIntervalsBetweenNearbyPeaks(peaks);
     console.log("intervalCounts.length = " + intervalCounts.length);
@@ -60,4 +64,19 @@ async function fingerprintProcess(e, sampleRate) {
         console.log(tempoCounts[0].tempo);
         return tempoCounts[0].tempo;
     } else return 0;*/
+}
+
+function getPeaksArrayAtThreshold(data, threshold, sampleRate) {
+    console.log("Called getPeaksAtThreshold with threshold " + threshold);
+    var peaksArray = [];
+    var length = data.length;
+    for (var i = 0; i < length;) {
+        //if (data[i] > threshold) {
+        peaksArray.push(data[i] > threshold ? 1 : 0);
+        // Skip forward ~ 1/4s to get past this peak.
+        i += sampleRate/100;
+        //}
+        //i++;
+    }
+    return peaksArray;
 }
